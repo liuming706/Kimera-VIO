5bf24d42ca226c6832e882f4fd02cdc4ac926a79
liuming <lm18816211581@163.com>
2025-06-28 12:19:09 +0800

    feat: 适配 noetic

--- scripts/docker/kimera_vio_ros_docker.bash
+++ scripts/docker/kimera_vio_ros_docker.bash
@@ -2,13 +2,14 @@
 
 # Allow X server connection
 xhost +local:root
-docker run -it --rm \
+docker run -it \
     --network=host \
     --env="DISPLAY" \
     --env="QT_X11_NO_MITSHM=1" \
     --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
     --volume="/media/lumen/CC897434BA39D0961/dataset:/data/datasets/Euroc:rw" \
-    --volume="~/workspace:~/workspace:rw" \
-    kimera_vio  kimera_vio
+    --volume="/home/lumen/workspace:/home/lumen/workspace:rw" \
+    --volume="/home/lumen/.ssh:/root/.ssh:rw" \
+    --name kimera_vio_ros kimera_vio_ros
 # Disallow X server connection
 xhost -local:root
